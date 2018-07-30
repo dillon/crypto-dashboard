80e04046ad32d901e1192a4f3bd10e3ed1d147ce
dpett122 <dpett122@gmail.com>
2018-07-29 21:31:51 -0400

    deleting logger

--- src/Store.js
+++ src/Store.js
@@ -7,11 +7,11 @@ import {
 import devTools from 'remote-redux-devtools';
 import promise from 'redux-promise';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 
 import RootReducer from './Reducers';
 
-const middleware = applyMiddleware(thunk, promise, logger);
+const middleware = applyMiddleware(thunk, promise);
 
 const Store = createStore(
     RootReducer,
